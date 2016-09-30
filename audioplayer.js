9327dea351f377e7bfbe918ea7c0e22c7b7c382f
Joe Grundman <Joe Grundman>
2016-09-30 11:08:04 +0200

    skip forward and timeline bindings working

--- audioplayer.js
+++ audioplayer.js
@@ -1,6 +1,7 @@
 /**
  * Obviously this is not the only way to do it, but i decided for a small app why bother
  * with jquery or angular when native js templating can handle it fine. It is versatile and easy.
+ * Could be useful to turn into a react component
  */
 
 //state object contains state of gplayer
@@ -11,21 +12,25 @@ const state = {
 
 // variable declarations
 var duration
-var ap
+var aPlayer
 var playhead
 var playListSelector
+var timeline
 
+function bindNewAudioPlayer () {
+    aPlayer = document.querySelector('#audio-player-console')
+    playhead = document.querySelector('#audio-player-playhead')
+    timeline = document.querySelector('#audio-player-timeline')
+    aPlayer.removeEventListener("timeupdate", timeUpdate)
+    aPlayer.removeEventListener("canplaythrough", setDuration)
+    aPlayer.load()
+    aPlayer.addEventListener("timeupdate", timeUpdate, false)
+    aPlayer.addEventListener("canplaythrough", setDuration, false)
+}
 
 /**
  * functions
  */
-
-//function accepts playList as first argument and id for second creating a playlist specific selector
-const listSelector = playList => id => {
-    state.currentTrackId = id
-    document.querySelector('#audio-player-hook').innerHTML = audioPlayer(playList[state.currentTrackId])
-    return
-}
 
 const setClassAsPlaying = (index) => {
     var playListCells = document.querySelectorAll('.audio-player-playlist-cell')
@@ -36,30 +41,36 @@ const setClassAsPlaying = (index) => {
     node.classList.add("playing")
 }
 
+const setDuration = () => duration = aPlayer.duration
+
 // update location of playhead
 const timeUpdate = () => {
-    console.log("duration", duration)
-    var playPercent = 100 * (ap.currentTime / duration)
-    console.log('playPercent', playPercent)
+    var playPercent = 100 * (aPlayer.currentTime / duration)
     playhead.style.marginLeft = playPercent + "%"
 }
 
+//function accepts playList as first argument and id for second creating a playlist specific selector
+const listSelectorAndBind = playList => id => {
+    state.currentTrackId = id
+
+    // attach new audio player, reattach event listeners and dom bindings
+    document.querySelector('#audio-player-hook').innerHTML = audioPlayer(playList[state.currentTrackId])
+    bindNewAudioPlayer()
+    return
+}
+
 const playAudio = () => {
-    // if(!ap){ap = document.querySelector('#audio-player-console')}   
-    if(ap.paused) {
-        ap.play()       
-    }
+    aPlayer.play()       
 }
 
 const pauseAudio = () => {
-    //  if(!ap){ap = document.querySelector('#audio-player-console')}
-     if (!ap.paused ) {
-         ap.pause()
-     }
+    aPlayer.pause()
+     
 }
 
 const forwardAudio = () => {
-
+    state.currentTrackId++
+    playListSelectorAndBind(state.currentTrackId)
 }
 
 
@@ -71,7 +82,7 @@ const forwardAudio = () => {
 const audioPlayerContainer = (data) => {
 
     // create playListSelector by passing in the playList to listSelector
-    playListSelector = listSelector(data.playList)
+    playListSelectorAndBind = listSelectorAndBind(data.playList)
 
     // render audio player 
     return (`<div class="audio-player-container">
@@ -88,16 +99,23 @@ const audioPlayerContainer = (data) => {
 // audio player and controls section
 const audioPlayer = (track) => (
     `<div class="audio-player">
-            <h3 id="audio-player-track-title"> Now Playing: ${track.name} </h3>
+            <p id="audio-player-track-title"> Now Playing: ${track.name} </p>
             <audio id="audio-player-console" src="${track.src}">
                 <p>Your browser does not support this audio player </p>
             </audio>
             <br>
-            <br>
-            <button id="audio-player-play-button" class="audio-player-main-button" onclick="playAudio()"><i class="ion-play"></i></button>
-            <button id="audio-player-pause-button" class="audio-player-main-button" onclick="pauseAudio()"><i class="ion-pause"></i></button>
-            <button id="audio-player-forward-button" class="audio-player-main-button" onclick="forwardAudio()"><i class="ion-skip-forward"></i></button>
-            <button id="audio-player-backward-button" class="audio-player-main-button" onclick="backAudio()"><i class="ion-skip-backward"></i></button>
+            <button id="audio-player-play-button" class="audio-player-main-button" onclick="playAudio()">
+                <i class="ion-play"></i>
+            </button>
+            <button id="audio-player-pause-button" class="audio-player-main-button" onclick="pauseAudio()">
+                <i class="ion-pause"></i>
+            </button>
+            <button id="audio-player-forward-button" class="audio-player-main-button" onclick="forwardAudio()">
+                <i class="ion-skip-forward"></i>
+            </button>
+            <button id="audio-player-backward-button" class="audio-player-main-button" onclick="backAudio()">
+                <i class="ion-skip-backward"></i>
+            </button>
             <br>
             <div id="audio-player-timeline-container">
                 <div id="audio-player-timeline">
@@ -120,17 +138,13 @@ const playList = (playList) => (`
         ${playList.map((track, i, playListArr) => (`
             <div class="audio-player-playlist-cell"
                 id="playlist-cell-${i}"" 
-                onclick="playListSelector(${i}); setClassAsPlaying(${i})">
+                onclick="playListSelectorAndBind(${i}); setClassAsPlaying(${i})">
             <p> Track: ${i + 1} - ${track.name}</p>
             </div>`)).join('')}
     </div>`)
 
 window.onload = function() {
-        playhead = document.querySelector('#audio-player-playhead')
-        ap = document.querySelector('#audio-player-console')
-        ap.addEventListener("timeupdate", timeUpdate, false)
-        console.log('duration', ap.duration)
-        ap.addEventListener("canplaythrough", () => {return duration = ap.duration}, false)
+        bindNewAudioPlayer()
 }
 
 
